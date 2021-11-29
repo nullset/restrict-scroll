5f7ab353874e00e417b58d715e034909215fc3d4
Nathan Wright <nathan@brandalism.com>
2021-11-29 16:37:32 -0600

    update demo page

--- public/demo.js
+++ public/demo.js
@@ -2,9 +2,16 @@ import restrictScroll from '../src/index.js';
 
 document.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
+    document
+      .querySelectorAll('button')
+      .forEach((node) => delete node.dataset.active);
+
     if (e.target.dataset.type) {
       const section = document.getElementById(e.target.dataset.type);
-      if (section) restrictScroll.add(section);
+      if (section) {
+        restrictScroll.add(section);
+        e.target.dataset.active = '';
+      }
     } else {
       restrictScroll.clear();
     }
